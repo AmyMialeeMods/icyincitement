8d260b2b05fd8cc32eb4b66d0ebd60e9b64f28e9
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2025-02-16 17:21:48 +0000

    final

--- src/main/java/xyz/amymialee/icyincitement/IcyIncitement.java
+++ src/main/java/xyz/amymialee/icyincitement/IcyIncitement.java
@@ -25,11 +25,11 @@ import xyz.amymialee.mialib.mvalues.MValueCategory;
 import xyz.amymialee.mialib.templates.MRegistry;
 
 public class IcyIncitement implements ModInitializer, EntityComponentInitializer {
-    public static String MOD_ID = "icyincitement";
-    public static MRegistry REGISTRY = new MRegistry(MOD_ID);
-    public static Item EMPTY_SPRINKLER = REGISTRY.register("empty_sprinkler", new Item.Settings().rarity(Rarity.UNCOMMON).maxCount(1), EmptySprinklerItem::new, ItemGroups.COMBAT);
-    public static Item SNOWBALL_SPRINKLER = REGISTRY.register("snowball_sprinkler", new Item.Settings().rarity(Rarity.EPIC).maxCount(1), SnowballSprinklerItem::new, ItemGroups.COMBAT);
-    public static EntityType<IceBallEntity> ICEBALL = REGISTRY.register("iceball", EntityType.Builder.<IceBallEntity>create(IceBallEntity::new, SpawnGroup.MISC).dropsNothing().dimensions(0.25F, 0.25F).maxTrackingRange(8).trackingTickInterval(2));
+    public static final String MOD_ID = "icyincitement";
+    public static final MRegistry REGISTRY = new MRegistry(MOD_ID);
+    public static final Item EMPTY_SPRINKLER = REGISTRY.register("empty_sprinkler", new Item.Settings().rarity(Rarity.UNCOMMON).maxCount(1), EmptySprinklerItem::new, ItemGroups.COMBAT);
+    public static final Item SNOWBALL_SPRINKLER = REGISTRY.register("snowball_sprinkler", new Item.Settings().rarity(Rarity.EPIC).maxCount(1), SnowballSprinklerItem::new, ItemGroups.COMBAT);
+    public static final EntityType<IceBallEntity> ICEBALL = REGISTRY.register("iceball", EntityType.Builder.<IceBallEntity>create(IceBallEntity::new, SpawnGroup.MISC).dropsNothing().dimensions(0.25F, 0.25F).maxTrackingRange(8).trackingTickInterval(2));
     public static final MValueCategory CATEGORY = new MValueCategory(id(MOD_ID), SNOWBALL_SPRINKLER, Identifier.ofVanilla("textures/block/ice.png"), 16, 16);
     public static final MValue<Integer> MAX_SNOWBALLS = MValue.of(id("max_snowballs"), MValue.INTEGER.of(128, 1, 256)).category(CATEGORY).item(Items.SNOW_BLOCK).build();
     public static final MValue<Double> RECHARGE_RATE = MValue.of(id("recharge_rate"), MValue.DOUBLE.of(0.45, 0d, 1d)).category(CATEGORY).item(Items.POWDER_SNOW_BUCKET).build();
